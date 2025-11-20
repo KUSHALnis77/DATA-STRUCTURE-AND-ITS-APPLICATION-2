54f125f4ebf1c594cebfb4658b9d490c15106899
KUSHALnis77 <shettykushal2005@gmail.com>
2025-11-20 12:32:20 +0530

    Add files via upload

---
+++ TOH.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+void toh(int n,char s,char d,char t)
+{
+    if(n>1)
+    {
+        toh(n-1,s,t,d);
+        printf("\nMove %d disc from %c to %c",n,s,d);
+        toh(n-1,t,d,s);
+    }
+    else
+        printf("\nMove %d disc from %c to %c",n,s,d);
+}
+int main()
+{
+    int n;
+    printf("\nRead number of discs: ");
+    scanf("%d",&n);
+    toh(n,'S','D','T');
+    return 0;
+}
+#include <stdlib.h>
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
